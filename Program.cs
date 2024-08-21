d9d8610037bff254036ea88f4eaec56bca26e22b
burak <burak@BURAK>
2024-08-21 11:07:56 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.WebSockets;
+
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter a number : ");
+            string input = Console.ReadLine();
+
+            int number = Convert.ToInt32(input);
+
+            int result;
+            result = number * number;
+            Console.WriteLine("Sqaure of the Number: " + result);
+
+        }
+
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Invalid Entry! Please enter a number");
+        }
+    }
+}
